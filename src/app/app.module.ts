e26161324c85550834106c485bec65c7b4ebff36
Tristan Caspers <tristancaspers97@hotmail.com>
2018-01-12 14:21:56 +0100

    Firebase setup & tslint config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {AngularFireModule} from "angularfire2";
+import {environment} from "../environments/environment";
+import {AngularFireDatabaseModule} from "angularfire2/database";
+import {AngularFireAuthModule} from "angularfire2/auth";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDb18Ao_BHfNuYNJL9NUzF5zDeW6zYh3v4",
+    authDomain: "gamehub-e3102.firebaseapp.com",
+    databaseURL: "https://gamehub-e3102.firebaseio.com",
+    projectId: "gamehub-e3102",
+    storageBucket: "gamehub-e3102.appspot.com",
+    messagingSenderId: "780932432548"
+  }
 };
